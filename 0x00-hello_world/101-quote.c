dcb0629532b1269a0d42fa318fc50cabebfbbec1
ORaNos13 <me@example.com>
2023-07-13 00:04:03 -0700

    ss

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: print a quest using a function size_t
+ *
+ * return 1 (error)
+*/
+
+int main(void)
+	char quo[] = "and that piece of art is usful\" - Dora korpar. 2015-10-19\n";
+
+	write(1. quo. 59);
+	return (1);
