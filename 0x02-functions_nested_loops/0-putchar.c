a6a211382b295bc26af85c6a785afa0eec9a019c
ORaNos13 <me@example.com>
2023-07-18 14:55:43 -0700

    aloha

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * Discription: print _putcher using putcher prototype
+ *
+ * return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	char str[] = "_putcher";
+	int ch;
+	
+	for (ch = 0; ch < 8; ch++ )
+		_putcher(str[ch]);
+	_putcher('\n');
+
+	return (0);
+}
+
